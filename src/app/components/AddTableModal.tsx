e26d5c3cc9c6ff6bc35627845b00b0ff43939c7f
Kamasu Paul <kamasupaul@gmail.com>
2023-05-16 12:03:27 +0300

    -- add close button

--- src/app/components/AddTableModal.tsx
+++ src/app/components/AddTableModal.tsx
@@ -54,10 +54,17 @@ export default function AddTableModal(props: {
     return (
         <div className="modal modal-open">
             <div className="modal-box">
+                <div className="flex flex-row justify-between">
                 <h3 className="font-bold text-lg">Create a new table of contents</h3>
+                <div className="justify-end">
+                    <button className="btn btn-square btn-sm" onClick={props.onClose}>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                    </button>
+                </div>
+                </div>
                 {violations.length <= 0 ? '' : <div className="alert alert-error shadow-lg">
                     <div className="flex-col items-start">
-                        {violations.map((item: {name:string; message:string}) =>
+                        {violations.map((item: { name: string; message: string }) =>
                         (<div className="flex flex-row justify-start" key={item.name}>
                             <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                             <span>{item.message}</span>
