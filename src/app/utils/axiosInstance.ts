995ce3363d1dffaff03a499a58ccb9d49adc7648
Kamasu Paul <kamasupaul@gmail.com>
2023-06-02 15:27:22 +0300

    -- add token to request

--- src/app/utils/axiosInstance.ts
+++ src/app/utils/axiosInstance.ts
@@ -1,7 +1,17 @@
-import axios,  { AxiosInstance } from 'axios';
-
+import axios, { AxiosInstance } from 'axios';
+import { auth } from '../firebase/app';
 const axiosInstance: AxiosInstance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BASE_URL
 });
-
+// Add a request interceptor
+axiosInstance.interceptors.request.use(async (config) => {
+    const user = auth.currentUser;
+    if (user) {
+        const token = await user.getIdToken();
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+    }
+    return config;
+});
 export default axiosInstance;
