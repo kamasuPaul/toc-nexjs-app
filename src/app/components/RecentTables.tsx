13391246f19bc3384fe9d83f2526c8fe5850a332
Kamasu Paul <kamasupaul@gmail.com>
2023-07-25 16:49:51 +0300

    -- make recent tables ssr

--- src/app/components/RecentTables.tsx
+++ src/app/components/RecentTables.tsx
@@ -1,23 +1,12 @@
-'use client';
-
-import { useEffect, useState } from "react";
 import TableSummary from "./TableSummary";
-import api from "../utils/axiosInstance";
+async function getTables(){
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/tables`,{ next: { revalidate: 30 } });
+    const tables = await response.json();
+    return tables;
+}
+export default async function RecentTables() {
+    const tables = await getTables();
 
-
-export default function RecentTables() {
-    const [tables, setTables] = useState([]);
-    useEffect(() => {
-        api.get('tables')
-            .then(response => {
-                const tables = response.data;
-                setTables(tables);
-            })
-            .catch(error => {
-                console.log(error);
-            })
-
-    }, [])
     return (
         <div className="grid grid-cols-1 md:grid-cols-4 gap-4 m-4">
             {tables.map((table: Table) => (<TableSummary key={table.id} table={table}></TableSummary>))}
